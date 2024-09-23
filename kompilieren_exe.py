5ff364ad628885d1e4c00f679ffe7c07eae6409c
Michael Crass <michaelcrass36@gmail.com>
2024-09-23 18:57:25 +0200

    icon

--- kompilieren_exe.py
+++ kompilieren_exe.py
@@ -3,7 +3,7 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'backup_tool.py',
     '--onefile',
-    '--icon=icon.ico',
+    '--icon=safe.ico',
     # '--onedir',
     # '-w'   #als demon = ohne shell
 ])
