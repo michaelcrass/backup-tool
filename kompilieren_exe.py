2e60cf570cf8a9b14ffbbd07ce19bb1bba9ec7f1
Michael Crass <michaelcrass36@gmail.com>
2024-09-24 23:19:14 +0200

    requirements

--- kompilieren_exe.py
+++ kompilieren_exe.py
@@ -1,5 +1,11 @@
 import PyInstaller.__main__
 
+# requirements:
+# pip install -r requirements.txt
+# py -m pip install pigar
+# # py -m pigar generate
+
+
 PyInstaller.__main__.run([
     'backup_tool.py',
     '--onefile',
